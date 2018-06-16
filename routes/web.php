6c3478694bceffc99ffe956a94e6e02916adccc1
Fernando <alvesfernandosantos1@gmail.com>
2018-06-16 11:39:45 -0300

    criando model e database city e states

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
 
+//One To Many
+Route::get('one-to-many', 'OneToManyController@oneToMany');
+
 Route::get('/', function () {
     return view('welcome');
 });
